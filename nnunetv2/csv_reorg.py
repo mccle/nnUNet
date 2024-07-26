aeb3c0326ed1559657d6bf4304bdc666928f00ee
Mason Cleveland <mcleveland2@mgh.harvard.edu>
2024-07-26 07:27:44 -0400

    add all_training flag

--- nnunetv2/csv_reorg.py
+++ nnunetv2/csv_reorg.py
@@ -5,10 +5,13 @@ from pathlib import Path
 from typing import Sequence
 from nnunetv2.dataset_conversion.generate_dataset_json import generate_dataset_json
 
-def reorganize(csv: Path | str, outdir: Path | str, dataset: str = "DATASET", fold: int = 0, im_keys: Sequence[str] = ["T1Post"]):
+def reorganize(csv: Path | str, outdir: Path | str, dataset: str = "DATASET", fold: int = 0, im_keys: Sequence[str] = ["T1Post"], train_all: bool = False):
     df = pd.read_csv(csv, dtype=str)
     df = df.sort_values(["Anon_PatientID", "Anon_StudyID"])
     df["nnUnetID"] = [f"{i:03d}" for i in range(df.shape[0])]
+
+    if train_all:
+        df[f"fold_{fold}"] = ["train"] * df.shape[0]
 
     for row in df.to_dict("records"):
         partition: str = row[f"fold_{fold}"]
@@ -40,6 +43,7 @@ def main():
     parser.add_argument("--dataset", type=str, default="DATASET")
     parser.add_argument("--im-keys", type=str, default="T1Post")
     parser.add_argument("--fold", type=int, default=0)
+    parser.add_argument("--train-all", action="store_true")
 
     args = parser.parse_args()
 
@@ -48,7 +52,8 @@ def main():
         "outdir": args.outdir,
         "dataset": args.dataset,
         "fold": args.fold,
-        "im_keys": args.im_keys.split(",") 
+        "im_keys": args.im_keys.split(","),
+        "train_all": args.train_all
     }
 
     reorganize(**kwargs)
