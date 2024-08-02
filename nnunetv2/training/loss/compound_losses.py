f2f7f47c6eb90b672cb13e5fe8c77cec127234f6
Mason Cleveland <mcleveland2@mgh.harvard.edu>
2024-08-02 12:58:02 -0400

    Add blob loss

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -1,8 +1,10 @@
 import torch
+import numpy as np
 from nnunetv2.training.loss.dice import SoftDiceLoss, MemoryEfficientSoftDiceLoss
 from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss, TopKLoss
 from nnunetv2.utilities.helpers import softmax_helper_dim1
 from torch import nn
+from cc3d import connected_components
 
 
 class DC_and_CE_loss(nn.Module):
@@ -54,6 +56,107 @@ class DC_and_CE_loss(nn.Module):
 
         result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
         return result
+
+# currently only for single class
+class blob_DC_and_CE_loss(nn.Module):
+    def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce=1, weight_dice=1, ignore_label=None,
+                 dice_class=SoftDiceLoss):
+        """
+        Weights for CE and Dice do not need to sum to one. You can set whatever you want.
+        :param soft_dice_kwargs:
+        :param ce_kwargs:
+        :param aggregate:
+        :param square_dice:
+        :param weight_ce:
+        :param weight_dice:
+        """
+        super(blob_DC_and_CE_loss, self).__init__()
+        if ignore_label is not None:
+            ce_kwargs['ignore_index'] = ignore_label
+
+        self.weight_dice = weight_dice
+        self.weight_ce = weight_ce
+        self.ignore_label = ignore_label
+
+        self.ce = RobustCrossEntropyLoss(**ce_kwargs)
+        self.dc = dice_class(apply_nonlin=softmax_helper_dim1, **soft_dice_kwargs)
+ 
+    # main loss logic called on each instance separately
+    def loss(self, net_output: torch.Tensor, target: torch.Tensor):
+        """
+        target must be b, c, x, y(, z) with c=1
+        :param net_output:
+        :param target:
+        :return:
+        """
+        if self.ignore_label is not None:
+            assert target.shape[1] == 1, 'ignore label is not implemented for one hot encoded target variables ' \
+                                         '(DC_and_CE_loss)'
+            mask = target != self.ignore_label
+            # remove ignore label from target, replace with one of the known labels. It doesn't matter because we
+            # ignore gradients in those areas anyway
+            target_dice = torch.where(mask, target, 0)
+            num_fg = mask.sum()
+        else:
+            target_dice = target
+            mask = None
+
+        dc_loss = self.dc(net_output, target_dice, loss_mask=mask) \
+            if self.weight_dice != 0 else 0
+        ce_loss = self.ce(net_output, target[:, 0]) \
+            if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
+
+        result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
+        return result
+
+    def forward(self, net_output: torch.Tensor, target: torch.Tensor):
+
+        global_loss = self.loss(net_output, target)
+        blob_loss = 0
+
+        for i in range(target.size(0)):
+            binary_output = (torch.sigmoid(net_output[i][0]) >= 0.5).type(torch.uint8).detach().numpy()
+            target_ccs = connected_components(target[i][0].detach().numpy())
+            output_ccs = connected_components(binary_output)
+
+            tracked_tumors = target_ccs.max()
+
+            cc_map = {}
+
+            for cc in np.unique(output_ccs):
+                if cc != 0:
+                    cc_mask = (output_ccs == cc).astype(int)
+
+                    tumor_id = (cc_mask * target_ccs).max()
+
+                    if tumor_id == 0:
+                        tracked_tumors += 1
+                        cc_map[cc] = tracked_tumors
+
+                    else:
+                        cc_map[cc] = tumor_id
+
+            vector_map = np.vectorize(lambda x: cc_map.get(x, x))
+            output_ccs = vector_map(output_ccs)
+
+            for cc in np.unique(target_ccs):
+                if cc != 0:
+                    output_mask = (output_ccs == cc).astype(int)
+                    target_mask = (target_ccs == cc).astype(int)
+
+                    masked_output = net_output[i].unsqueeze(0) * torch.tensor(output_mask, device=net_output.device)
+                    masked_target = target[i].unsqueeze(0) * torch.tensor(target_mask, device=target.device)
+
+                    blob_loss += self.loss(masked_output, masked_target)
+
+    
+
+        return (1 * global_loss) + (2 * blob_loss)
+
+
+
+
+
 
 
 class DC_and_BCE_loss(nn.Module):
